4d8ef3e16def0bbfb8054dc5dfd3f6f6d0519ea9
parleonico <nicolasparola@gmail.com>
2021-10-30 12:41:12 -0300

    Desafio1

---
+++ Clase 1/Desafio1/script.js
@@ -0,0 +1,10 @@
+//Compra de supermercado
+let item1 = parseInt(prompt("Ingrese el costo del item 1"))
+let item2 = parseInt(prompt("Ingrese el costo del item 2"))
+let item3 = parseInt(prompt("Ingrese el costo del item 3"))
+let item4 = parseInt(prompt("Ingrese el costo del item 4"))
+let item5 = parseInt(prompt("Ingrese el costo del item 5"))
+let item6 = parseInt(prompt("Ingrese el costo del item 6"))
+
+alert("Usted gastó $" +(item1+item2+item3+item4+item5+item6))
+alert("El promedio de los items fue de $" +(item1+item2+item3+item4+item5+item6)/6)
